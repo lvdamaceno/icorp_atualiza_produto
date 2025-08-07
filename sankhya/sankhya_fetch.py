816a41652e4596357769ff82ebcf6178b370084f
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2025-08-07 16:41:13 -0300

    Set order codprod

--- sankhya/sankhya_fetch.py
+++ sankhya/sankhya_fetch.py
@@ -72,7 +72,7 @@ def sankhya_list_total_codprod(token, batch_size=5000):
                 WHERE ATIVO = 'S'
                   AND USOPROD = 'R'
                   AND CODGRUPOPROD <= '1159999'
-                  AND CODPROD > 28950
+                ORDER BY CODPROD
             ) AS T
             WHERE RN BETWEEN {start} AND {end}
         """

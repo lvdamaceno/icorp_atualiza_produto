f43a2648a503d2bd17f3d3bdcfaaff6bda6160a1
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2025-11-04 16:35:22 -0300

    Full update

--- updates/total_update.py
+++ updates/total_update.py
@@ -2,41 +2,43 @@ from sankhya.sankhya_client import login
 from sankhya.sankhya_fetch import sankhya_list_total_codprod
 from updates.process import process_integration
 from utils import logging_config
+import logging
+from concurrent.futures import ProcessPoolExecutor, as_completed
 
 logging_config()
 
-from concurrent.futures import ThreadPoolExecutor, as_completed
+# if __name__ == "__main__":
+#     token = login()
+#     products = sankhya_list_total_codprod(token, 1, 3000)
+#     process_integration(token, products)
 
-def main():
-    token = login()
+def run_lote(start, end):
+    token = login()  # cada processo faz seu login independente
+    products = sankhya_list_total_codprod(token, start, end)
+    process_integration(token, products)
+    return f"Lote {start}-{end} finalizado com {len(products)} produtos"
 
-    # define os intervalos (start, end) de cada lote
-    lotes = [(1, 2000), (2001, 4000), (4001, 6000), (6001, 8000), (8001, 10000), (10001, 12000), (12001, 14000),
-             (14001, 16000), (16001, 18000), (18001, 20000)]
+if __name__ == "__main__":
+    lotes = [
+        (1, 2000),
+        (2001, 4000),
+        (4001, 6000),
+        (6001, 8000),
+        (8001, 10000),
+        (10001, 12000),
+        (12001, 14000),
+        (14001, 16000),
+        (16001, 18000),
+        (18001, 20000)
+    ]
 
-    results = []
+    with ProcessPoolExecutor(max_workers=len(lotes)) as executor:
+        futures = {executor.submit(run_lote, start, end): (start, end) for start, end in lotes}
 
-    # cria um pool de threads (2 nesse caso)
-    with ThreadPoolExecutor(max_workers=10) as executor:
-        # dispara as chamadas em paralelo
-        futures = {
-            executor.submit(sankhya_list_total_codprod, token, start, end): (start, end)
-            for start, end in lotes
-        }
-
-        # espera cada lote terminar
         for future in as_completed(futures):
             start, end = futures[future]
             try:
-                products = future.result()
-                print(f"Lote {start}-{end} retornou {len(products)} produtos")
-                results.extend(products)
+                result = future.result()
+                logging.info(result)
             except Exception as e:
-                print(f"Erro no lote {start}-{end}: {e}")
-
-    # agora processa a integração depois que ambos terminaram
-    process_integration(token, results)
-
-
-if __name__ == "__main__":
-    main()
+                logging.info(f"Erro no lote {start}-{end}: {e}")
